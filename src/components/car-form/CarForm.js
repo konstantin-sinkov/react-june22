35054dc43b3b4a09b8d8ba8deeecd0f9fcac0f51
konstantin-sinkov <sinkov.ks@gmail.com>
2022-09-30 11:15:34 +0300

    homework 3-2 update car method in progress

--- src/components/car-form/CarForm.js
+++ src/components/car-form/CarForm.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
 import {joiResolver} from '@hookform/resolvers/joi';
 import {carValidator} from "../../validators";
 import {carsService} from "../../services/cars.service";
 
-const CarForm = ({setCars}) => {
+const CarForm = ({setCars, carForUpdate}) => {
     const {register, handleSubmit, setValue, formState: {errors, isValid}} = useForm({
         resolver: joiResolver(carValidator),
         mode: "all"
     }); //mode indicates when result obj will be sent to
     
+    useEffect(() => {
+        if (carForUpdate) {
+            console.log(carForUpdate);
+            setValue('model', carForUpdate.model, {shouldValidate: false});
+            setValue('year', carForUpdate.year, {shouldValidate: false});
+            setValue('price', carForUpdate.price, {shouldValidate: false});
+        }
+    }, [carForUpdate, setValue]);
+    
+    
     const submit = async (car) => {
-        // carsService.addCar(car).then(console.log(car));
-        const {data} = await carsService.addCar(car);
-        setCars(cars => [...cars, car]); //викликаємо сеттер і передаємо в нього старе значення cars + {car}, setState слідкує за Cars і перерендерить сторінку
-        // console.log(data);
+        if (carForUpdate) {
+            await carsService.editCar(carForUpdate.id, carForUpdate);
+            // cars.findIndex(carForUpdate.id);
+        } else {
+            // carsService.addCar(car).then(console.log(car));
+            await carsService.addCar(car);
+            setCars(cars => [...cars, car]); //викликаємо сеттер і передаємо в нього старе значення cars + {car}, setState слідкує за Cars і перерендерить сторінку
+            // console.log(data);
+            }
     }
     
     return (
@@ -25,13 +40,14 @@ const CarForm = ({setCars}) => {
             </div>
             <div>
                 <input type="text" placeholder={'year'} {...register('year', /*{valueAsNumber: true}*/)}/>
+                {/*{carForUpdate && setValue('')}*/}
                 {errors.year &&  <span>{errors.year.message}</span>}
             </div>
             <div>
                 <input type="text" placeholder={'price'} {...register('price', /*{valueAsNumber: true}*/)}/>
                 {errors.price &&  <span>{errors.price.message}</span>}
             </div>
-            <input type="submit"/>
+            <button onClick={submit}>{carForUpdate ? 'Save edited car' : 'Add new car'}</button>
         </form>
     );
 }
