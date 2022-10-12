00f94e3d43135e3106d4b0dc11041e421f5ccd4e
konstantin-sinkov <sinkov.ks@gmail.com>
2022-10-12 16:30:46 +0300

    use AsyncThunk v0.5

--- src/redux/slices/user.slice.js
+++ src/redux/slices/user.slice.js
@@ -1,4 +1,6 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk} from "@reduxjs/toolkit/src/createAsyncThunk";
+import {usersService} from "../../services";
 
 const initialState = {
     users: [],
@@ -7,20 +9,56 @@ const initialState = {
     error: null
 }
 
+const getUsers = createAsyncThunk(
+    'userSlice/getUsers', //2 params: 1st sliceName/function, 2nd - async callback
+    async (_, {rejectWithValue}) => { //async cb has 2 params, 1st - what we
+        try {
+            const {data} = await usersService.getUsers();
+            return data; //result of this request will be obroblenyi by the extraReducer
+        }
+        catch (e) {
+            return rejectWithValue(e.response.data);
+        }
+    }
+);
+
+createAsyncThunk(
+    'userSlice/getUserByID',
+    async ({id}, {rejectedWithValue}) => {
+        try {
+            const {data} = await usersService.getUserByID(id);
+            return data;
+        } catch (e) {
+            return rejectedWithValue(e.response.data);
+        }
+    }
+);
+
+
 const userSlice = createSlice({
     name: 'userSlice',
     initialState,
     reducers: {
-        getUsers: (state, action) => {
-            state.users = action.payload
-        },
+        // getUsers: (state, action) => {
+        //     state.users = action.payload
+        // },
         setCurrentUser: (state, action) => {
             state.currentUser = action.payload
         }
-    }
+    },
+    // extraReducers: {
+    //     [getUsers.fulfilled]: (state, action) => {
+    //         state.users = action.payload;
+    //     }
+    // },
+    extraReducers: builder =>
+        builder
+            .addCase(getUsers.fulfilled, (state, action) => {
+                state.users = action.payload
+            })
 });
 
-const {reducer: userReducer, actions: {getUsers, setCurrentUser}} = userSlice;
+const {reducer: userReducer, actions: {setCurrentUser}} = userSlice;
 
 const userActions = {
     getUsers,
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -2,7 +2,8 @@ import {axiosService} from "./axios.service";
 import {urls} from "../configs";
 
 const usersService = {
-    getUsers: () => axiosService.get(urls.users)
+    getUsers: () => axiosService.get(urls.users),
+    getUserByID: (id) => axiosService.get(`${urls.users}/${id}`)
 }
 
 export {
